2e2047e016ae5b85df74a70aca34ace630dacb17
Nguyễn Đức Thiện <nguyenducthien9@gmail.com>
2025-02-26 07:28:05 +0000

    feat: add umami analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Script defer src="https://cloud.umami.is/script.js" data-website-id="833cb692-88c7-467a-8cf7-ffec21b39bd4" />
     </html>
   );
 }
