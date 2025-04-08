53028752d66ed41fbd21aa8f3d3be3aa904ef98a
iuuukhueeee <nguyenducthien9@gmail.com>
2025-04-08 23:28:03 +0700

    feat: add next analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
