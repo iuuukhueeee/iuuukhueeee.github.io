f60aeb1b26857141de10720d037a5d8c64403cad
iuuukhueeee <nguyenducthien9@gmail.com>
2025-04-13 23:44:16 +0700

    feat: ignore frontmatter while parsing

--- src/app/lib/markdownToHtml.ts
+++ src/app/lib/markdownToHtml.ts
@@ -3,6 +3,7 @@ import rehypeHighlight from "rehype-highlight";
 import rehypeParse from "rehype-parse";
 import rehypeReact from "rehype-react";
 import rehypeStringify from "rehype-stringify";
+import remarkFrontmatter from "remark-frontmatter";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import { unified } from "unified";
@@ -19,6 +20,7 @@ export default async function markdownToHtml(markdown: string) {
   const file = await unified()
     .use(remarkParse)
     .use(remarkRehype)
+    .use(remarkFrontmatter)
     .use(rehypeFormat)
     .use(rehypeStringify)
     .process(markdown)
