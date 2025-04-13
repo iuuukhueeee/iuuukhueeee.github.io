9ccfb41f3ec6c8378dae9f09b3db4dcaee5880ac
iuuukhueeee <nguyenducthien9@gmail.com>
2025-04-13 23:40:10 +0700

    fix: build error due to missing package

--- src/app/lib/markdownToHtml.ts
+++ src/app/lib/markdownToHtml.ts
@@ -3,7 +3,6 @@ import rehypeHighlight from "rehype-highlight";
 import rehypeParse from "rehype-parse";
 import rehypeReact from "rehype-react";
 import rehypeStringify from "rehype-stringify";
-import remarkFrontmatter from "remark-frontmatter";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import { unified } from "unified";
@@ -20,7 +19,6 @@ export default async function markdownToHtml(markdown: string) {
   const file = await unified()
     .use(remarkParse)
     .use(remarkRehype)
-    .use(remarkFrontmatter)
     .use(rehypeFormat)
     .use(rehypeStringify)
     .process(markdown)
