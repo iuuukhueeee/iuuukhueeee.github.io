f1d764abcc6905bd732bba5e44879e9e2436ad92
iuuukhueeee <nguyenducthien9@gmail.com>
2025-12-20 21:46:04 +0700

    feat: generating og image

---
+++ src/app/posts/[slug]/opengraph-image.tsx
@@ -0,0 +1,39 @@
+import { ImageResponse } from 'next/og'
+ 
+export const size = {
+  width: 1200,
+  height: 630,
+}
+export const contentType = 'image/png'
+ 
+export default async function Image({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
+  const post = await fetch(`https://.../posts/${slug}`).then((res) =>
+    res.json()
+  )
+ 
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 48,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {post.title}
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
+}
