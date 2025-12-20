4b4cd14c9548a60151da712323d7871804030c90
iuuukhueeee <nguyenducthien9@gmail.com>
2025-12-20 21:54:58 +0700

    chore: update render from static to dynamic

--- src/app/posts/[slug]/opengraph-image.tsx
+++ src/app/posts/[slug]/opengraph-image.tsx
@@ -1,32 +1,32 @@
-import { ImageResponse } from 'next/og'
- 
+import { ImageResponse } from "next/og";
+
 export const size = {
   width: 1200,
   height: 630,
-}
-export const contentType = 'image/png'
- 
+};
+export const contentType = "image/png";
+
 export default async function Image({
   params,
 }: {
-  params: Promise<{ slug: string }>
+  params: Promise<{ slug: string }>;
 }) {
-  const { slug } = await params
+  const { slug } = await params;
   const post = await fetch(`https://.../posts/${slug}`).then((res) =>
     res.json()
-  )
- 
+  );
+
   return new ImageResponse(
     (
       <div
         style={{
           fontSize: 48,
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
         {post.title}
@@ -35,5 +35,5 @@ export default async function Image({
     {
       ...size,
     }
-  )
+  );
 }
