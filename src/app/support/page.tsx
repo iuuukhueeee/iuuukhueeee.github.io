f470980ea8380eb59d6052f473e448328121f043
iuuukhueeee <nguyenducthien9@gmail.com>
2025-12-23 17:22:19 +0700

    feat: add support page

---
+++ src/app/support/page.tsx
@@ -0,0 +1,8 @@
+export default function Support() {
+  return (
+    <p className="flex items-center justify-center h-screen">
+      For technical support and any other needs, email me
+      at nguyenducthien9.work@gmail.com
+    </p>
+  );
+}
